22e2b93131f52d3db0f2b70d0b32f276de24f64f
Ashvat Bansal <ashvatb2@linux-a3.ews.illinois.edu>
2020-08-28 12:41:37 -0500

    Pushing Lab1 Part1

--- Lab1/blackbox_tb.v
+++ Lab1/blackbox_tb.v
@@ -1,3 +1,33 @@
 module blackbox_test;
 
+	reg o, v, l;
+
+	wire a;
+
+	blackbox bx1(a, o, v, l);
+
+
+	initial begin
+
+		$dumpfile("blackbox.vcd");
+		$dumpvars(0, blackbox_test);
+
+		o = 0; v = 0; l = 0; # 10;
+		o = 0; v = 0; l = 1; # 10;
+		o = 0; v = 1; l = 0; # 10;
+		o = 0; v = 1; l = 1; # 10;
+		o = 1; v = 0; l = 0; # 10;
+		o = 1; v = 0; l = 1; # 10;
+		o = 1; v = 1; l = 0; # 10;
+		o = 1; v = 1; l = 1; # 10;
+
+		$finish;
+
+	end
+
+	initial begin
+		$display("inputs = o, v, l  outputs = a");
+		$monitor("At time %2t, a = %d o = %d v = %d l = %d", $time, a, o, v, l);
+	end
+
 endmodule // blackbox_test
