7fc533899dba465a6db38f1e6f9fddcd70ad9b83
Ashvat Bansal <ashvatb2@illinois.edu>
2020-08-28 16:32:03 -0500

    Keypad Valid Input Complete

--- Lab1/keypad.v
+++ Lab1/keypad.v
@@ -1,6 +1,17 @@
 module keypad(valid, number, a, b, c, d, e, f, g);
-   output 	valid;
+
+   // inputs, outputs and wires
+   output valid;
    output [3:0] number;
-   input 	a, b, c, d, e, f, g;
+   input  a, b, c, d, e, f, g;
+   wire   w1, w2;
+
+   // Determining valid key press
+   and andAG(w1, a, g); // Key press left of 0
+   and andCG(w2, c, g); // Key press right of 0
+   or Valid(valid, w1, w2, b, d, e, f);
+
+   // Assigning number
+
 
 endmodule // keypad
