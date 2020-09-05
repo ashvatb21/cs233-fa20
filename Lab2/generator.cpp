048090a6b45afe6974f8ea53a1c8f3279600d22c
Ashvat Bansal <ashvatb2@illinois.edu>
2020-09-05 14:38:07 -0500

    Forgot to add generator code

--- Lab2/generator.cpp
+++ Lab2/generator.cpp
@@ -3,3 +3,15 @@
 
 // make generator
 // ./generator
+
+#include <cstdio>
+using std::printf;
+
+int main()
+{
+    int width = 32;
+    for (int i = 1; i < width; i++)
+    {
+        printf("alu1 a%d(out[%d], Cout[%d], A[%d], B[%d], Cout[%d], control);\n", i, i, i, i, i, i - 1);
+    }
+}
