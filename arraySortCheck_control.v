8a5316368bd98c002d66dd56165b2742286f7953
Meyers, Sofia Emmanuilovna <sofiaem2@illinois.edu>
2020-09-06 15:08:14 -0500

    Delete arraySortCheck_control.v

--- arraySortCheck_control.v
+++
@@ -1,7 +0,0 @@
-
-module arraySortCheck_control(sorted, done, load_input, load_index, select_index, go, inversion_found, end_of_array, zero_length_array, clock, reset);
-	output sorted, done, load_input, load_index, select_index;
-	input go, inversion_found, end_of_array, zero_length_array;
-	input clock, reset;
-
-endmodule
