a21e499a8bf3208a3df7fa1ffd7f396e85180814
Ashvat Bansal <ashvatb2@illinois.edu>
2020-09-30 12:09:36 -0500

    Waiting to check the solutin for part 1

--- Lab6/p1.s
+++ Lab6/p1.s
@@ -12,4 +12,40 @@
 # }
 .globl fill_array
 fill_array:
+
+    li $t0, 0                   # int i = 0
+    li $t9, 3                   # arbitrary register storing integer 3
+
+for_loop:
+
+    bge $t0, 6, end_loop        # loop condition
+
+    srl $a0, $a0, 3             # request >>= 3
+    rem $t1, $t0, $t9           # i % 3
+
+    bne $t1, 0, else            # if (i % 3 == 0)
+
+    mul $t2, $t0, 4             # 4*i 
+    add $t3, $a1, $t2           # integer offset for array
+
+    lw $t4, 0($t3)              # storing array[i]
+    and $t4, $a0, 0x0000001f    # array[i] = request & 0x0000001f
+    sw $t4, 0($t3)              # storing array[i] in memory
+
+    add $t0, $t0, 1             # i++
+
+    j for_loop
+
+else:
+
+    lw $t4, 0($t3)              # storing array[i]
+    and $t4, $a0, 0x000000ff    # array[i] = request & 0x000000ff
+    sw $t4, 0($t3)              # storing array[i] in memory
+
+    add $t0, $t0, 1             # i++
+
+    j for_loop
+
+
+end_loop:
     jr      $ra
